23dcae5a59108c91f300952bd4a8e4d3d9ed8160
DeepSOIC <vv.titov@gmail.com>
2021-12-10 19:54:12 +0300

    invert EN signal
    wasn't right

--- pla-firmware/defaults.h
+++ pla-firmware/defaults.h
@@ -51,7 +51,7 @@
   #define DEFAULT_JUNCTION_DEVIATION 0.01 // mm
   #define DEFAULT_ARC_TOLERANCE 0.02 // mm
   #define DEFAULT_REPORT_INCHES 0 // false
-  #define DEFAULT_INVERT_ST_ENABLE 0 // false
+  #define DEFAULT_INVERT_ST_ENABLE 1 // false
   #define DEFAULT_INVERT_LIMIT_PINS 0 // false
   #define DEFAULT_SOFT_LIMIT_ENABLE 0 // false
   #define DEFAULT_HARD_LIMIT_ENABLE 0  // false
